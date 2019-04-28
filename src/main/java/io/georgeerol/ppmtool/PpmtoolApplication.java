1b6241e747545031863c8147079e2ef1a1a9c31f
George Fouche <fouliex@gmail.com>
2019-04-28 15:06:13 -0700

    Initial commit

---
+++ src/main/java/io/georgeerol/ppmtool/PpmtoolApplication.java
@@ -0,0 +1,13 @@
+package io.georgeerol.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpmtoolApplication.class, args);
+    }
+
+}
